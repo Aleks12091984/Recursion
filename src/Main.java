a8c9f56f90dbd8f68bc89f3ddbd2aae05fcd2514
Aleks <aleksey-mazur-7@mail.ru>
2023-09-27 18:47:33 +0300

    recurs

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.print("Hello world!");
+        //main(null);
+
+        Scanner kb = new Scanner(System.in);
+       /* System.out.print("Введите номер этажа: ");
+        int floor = kb.nextInt();
+        Elevator(floor);*/
+        /*System.out.print("Введите число для вычисления факториала: ");
+        int n = kb.nextInt();
+        for(int i = 0; i <= n; i++)
+        System.out.printf("%d! = %e\n", i ,Factorial(i));*/
+
+        /*System.out.print("Введите основание степени: ");
+        int a = kb.nextInt();
+        System.out.print("Введите показатель степени: ");
+        int n = kb.nextInt();
+        System.out.println("%d ^ %d = %f".formatted(a, n, Power(a,n)));*/
+
+        System.out.print("Введите предельное число: ");
+        int n = kb.nextInt();
+        Fibonacci(n, 0, 1);
+
+    }
+
+    static void Fibonacci(int n, int a, int b)
+    {
+        if(a > n)return;
+        System.out.print(a + "\t");
+        Fibonacci(n, b, a + b);
+    }
+
+    static double Power(double a, int n)
+    {
+        /*if(n == 0)return 1;
+        else if(n > 0)return a * Power(a, n-1);
+        else return 1 / a * Power(a, n + 1);*/
+        //return n == 0 ? 1 : n > 0 ? a*Power(a, n - 1) : 1 / a * Power(a, n + 1);
+        return n == 0 ? 1 : n > 0 ? a*Power(a, n - 1) : 1 /Power(a, -n);
+        //return n == 0 ? 1 : n > 0 ? a*Power(a, n - 1) : 1 /Power(a, Math.abs(n));
+
+    }
+    static double Factorial(int n)
+    {
+        /*if(n < 0)return 0;
+        if(n == 0)return 1;
+        else return n*Factorial(n-1);*/
+        return n < 0 ? 0. : n == 0 ? 1. : n*Factorial(n - 1);
+    }
+    static void Elevator(int floor)
+    {
+        if(floor == 0)
+        {
+            System.out.println("Вы в подвале");
+            return;
+        }
+        System.out.println("Вы на " + floor + " этаже");
+        Elevator(floor-1);
+        System.out.println("Вы на " + floor + " этаже");
+    }
+}
